1291b55587c871677f420bb120a2b13456a676b5
thayna-oliveira <oliveira.thayna@outlook.com>
2023-08-13 22:59:37 +0100

    update search bar to add loading state

--- src/app/shared/search-bar/search-bar.component.ts
+++ src/app/shared/search-bar/search-bar.component.ts
@@ -14,6 +14,7 @@ import { ControlContainer, FormGroupDirective } from '@angular/forms';
 })
 export class SearchBarComponent {
   @Input() controlName: string;
+  @Input() loading: boolean;
   @Input() placeholder: string = 'Search...';
   @Output() onClickEvent = new EventEmitter();
 
