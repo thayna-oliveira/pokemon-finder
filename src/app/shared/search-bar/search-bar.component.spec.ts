f6f728d8c9ed6879d6018c687ee7301e0e066776
thayna-oliveira <oliveira.thayna@outlook.com>
2023-08-13 16:34:24 +0100

    add search bar component

---
+++ src/app/shared/search-bar/search-bar.component.spec.ts
@@ -0,0 +1,45 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  FormControl,
+  FormGroup,
+  FormGroupDirective,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { SearchBarComponent } from './search-bar.component';
+
+describe('SearchBarComponent', () => {
+  let component: SearchBarComponent;
+  let fixture: ComponentFixture<SearchBarComponent>;
+
+  const fgd: FormGroupDirective = new FormGroupDirective([], []);
+
+  fgd.form = new FormGroup({
+    test: new FormControl(''),
+  });
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule],
+      declarations: [SearchBarComponent],
+      providers: [{ provide: FormGroupDirective, useValue: fgd }],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(SearchBarComponent);
+    component = fixture.componentInstance;
+    component.controlName = 'test';
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should emit click event on button click', () => {
+    const buttonSpy = jest.spyOn(component.onClickEvent, 'emit');
+    const button = fixture.debugElement.query(By.css('.search-icon'));
+
+    button.nativeElement.click();
+    expect(buttonSpy).toHaveBeenCalledTimes(1);
+  });
+});
---
+++ src/app/shared/search-bar/search-bar.component.ts
@@ -0,0 +1,23 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ControlContainer, FormGroupDirective } from '@angular/forms';
+
+@Component({
+  selector: 'app-search-bar',
+  templateUrl: './search-bar.component.html',
+  styleUrls: ['./search-bar.component.scss'],
+  viewProviders: [
+    {
+      provide: ControlContainer,
+      useExisting: FormGroupDirective,
+    },
+  ],
+})
+export class SearchBarComponent {
+  @Input() controlName: string;
+  @Input() placeholder: string = 'Search...';
+  @Output() onClickEvent = new EventEmitter();
+
+  handleClickEvent(): void {
+    this.onClickEvent.emit();
+  }
+}
